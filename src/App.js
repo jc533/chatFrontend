c2d83e35449920a6103574fee6c0c03c023a8794
justinChen <yub612@gmail.com>
2021-08-09 02:08:38 +0800

    remove some files and dirs

--- src/App.js
+++ src/App.js
@@ -1,33 +1,13 @@
 import React from "react";
 import {useState} from "react";
-import Login from "./pages/login.js";
-import Register from "./pages/register.js";
-import Main from "./pages/main.js";
 //import { Link } from "react-router"
 import {BrowserRouter as Router, Redirect, Link, Switch, HashRouter, Route} from "react-router-dom";
 import "./stylesheet/style.css";
 //import "./App.css";
 import Routes from "./route.js";
-import SingIn from "./views";
-import {login} from "actions"
 
 function App() {
 	return <Routes/>
-  /*return (
-	<Router>
-		<Switch>
-			<Route path="/login">
-				<Login/>
-			</Route>
-			<Route path="/register">
-				<Register/>
-			</Route>
-			<Route exact path="/">
-				<Main/>
-			</Route>
-		</Switch>
-	</Router>
-  );*/
 }
 
 export default App;
